0eec508d4ab997ec00ad2a921b83644dfb71061f
Juan Vega <jdavidvf1@gmail.com>
2020-06-26 12:45:25 -0500

    Insertar información y visualización de la misma termanada

--- app.js
+++ app.js
@@ -8,25 +8,57 @@ class Product{
 }
 class UI{
     addProduct(product){
-        const ProductList=document.getElementById("product-list");
+        const ProductList=document.getElementById("Product list");
+        const element = document.createElement("div");
+        element.innerHTML= `
+            <div class ="card txt-center mb-4">
+                <div class="card-body">
+                <strong>Product</strong>: ${product.name}
+                <strong>Product Price</strong>:${product.price}
+                <strong>Product year</strong>:${product.year}
+               <a href="#" class="btn btn-danger" name="delete">Eliminar</a>
+                </div>
+            </div>
+        `;  
+        ProductList.appendChild(element);
+       this.resetForm();
     }
-    deleteProduct(){
+    resetForm(){
+        document.getElementById("product-form").reset();
+    }
+
+    deleteProduct(element){
+
+        if(element.name==="delete"){
+               element.parentElement.parentElement.parentElement.remove();
+        }
 
     }
-    showMessage(){
+    showMessage(message, cssClass){
+       const div= document.createElement("div");
+       div.className=`alert alert-${cssClass}`;
+       div.appendChild(document.createTextNode(message));
+       
 
     }
 }
 //DOM
 document.getElementById("product-form").addEventListener("submit", function(e){
+    e.preventDefault(); 
     const name=document.getElementById("name").value
     const price=document.getElementById("price").value
     const year=document.getElementById("year").value
 
-    console.log("nombre: "+name +"\n price: "+price+"\n year: "+year)
 
-   const product= new Product(name,price,year);
-
-    e.preventDefault();
+    const product= new Product(name,price,year);
+    const ui=new UI();
+    ui.addProduct(product)
+   
+  
+    
 
 })
+document.getElementById("Product list").addEventListener("click", function(e){
+const ui=new UI();
+ui.deleteProduct(e.target);
+});
